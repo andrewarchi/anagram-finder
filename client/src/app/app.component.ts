f05d34dc9154542d2353f71587a297fbffba86bf
Andrew Archibald <andrew@robarchibald.com>
2017-06-19 17:31:58 -0700

    Add client anagram viewer

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: []
 })
 export class AppComponent {
-  title = 'app';
+  _letters: string;
+  anagrams: string[];
+
+  constructor(private http: Http) {}
+
+  get letters(): string {
+    return this._letters;
+  }
+  set letters(letters: string) {
+    this.http.get('http://localhost:3141/anagrams/' + letters)
+      .subscribe(res => this.anagrams = res.json());
+    this._letters = letters;
+  }
 }
